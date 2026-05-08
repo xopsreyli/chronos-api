110bf532276873bc9cdac079d1eb93d0bad1a521
xopsreyli <xopsreyli@gmail.com>
2026-05-08 19:05:25 +0300

    Fixed user controller

--- src/controllers/user/current/controller.ts
+++ src/controllers/user/current/controller.ts
@@ -1,13 +1,9 @@
-import type { Request, Response } from 'express'
+import type {Request, Response} from 'express'
 import * as service from '../../../services/user/current/service.js'
 import statusCodes from '../../../enums/response/statusCodes/enums.js'
-import type {
-    NicknameUpdationData,
-    PasswordUpdationData,
-    UserPublic,
-} from '../../../types/user/types.js'
-import type { Settings } from '../../../generated/prisma/client.js'
-import type { SettingsUpdationData } from '../../../types/settings/types.js'
+import type {NicknameUpdationData, PasswordUpdationData, UserPublic,} from '../../../types/user/types.js'
+import type {Settings} from '../../../generated/prisma/client.js'
+import type {SettingsUpdationData} from '../../../types/settings/types.js'
 
 const getUser = async (req: Request, res: Response) => {
     const id: number = req.userId!
@@ -29,7 +25,7 @@ const updatePassword = async (req: Request, res: Response) => {
     const data: PasswordUpdationData = req.body
     await service.updatePassword(id, data)
 
-    res.status(statusCodes.NO_CONTENT)
+    res.sendStatus(statusCodes.NO_CONTENT)
 }
 
 const getSettings = async (req: Request, res: Response) => {
@@ -47,4 +43,4 @@ const updateSettings = async (req: Request, res: Response) => {
     res.status(statusCodes.OK).json(settings)
 }
 
-export { getUser, updateNickname, updatePassword, getSettings, updateSettings }
+export {getUser, updateNickname, updatePassword, getSettings, updateSettings}
