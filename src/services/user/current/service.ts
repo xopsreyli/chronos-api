f2f5d7233d8c0156e5b011203de70ec77774c272
xopsreyli <xopsreyli@gmail.com>
2026-05-08 15:39:15 +0300

    fixed updatePassword logic

--- src/services/user/current/service.ts
+++ src/services/user/current/service.ts
@@ -1,16 +1,12 @@
 import prisma from '../../../lib/prisma/prisma.js'
-import type { Settings, User } from '../../../generated/prisma/client.js'
-import type {
-    NicknameUpdationData,
-    PasswordUpdationData,
-    UserPublic,
-} from '../../../types/user/types.js'
-import type { SettingsUpdationData } from '../../../types/settings/types.js'
+import type {Settings, User} from '../../../generated/prisma/client.js'
+import type {NicknameUpdationData, PasswordUpdationData, UserPublic,} from '../../../types/user/types.js'
+import type {SettingsUpdationData} from '../../../types/settings/types.js'
 import bcrypt from 'bcrypt'
 import AppError from '../../../utils/AppError/AppError.js'
 import statusCodes from '../../../enums/response/statusCodes/enums.js'
 import errorMessages from '../../../enums/error/messages/enums.js'
-import { SALT_ROUNDS } from '../../../enums/bcrypt/enums.js'
+import {SALT_ROUNDS} from '../../../enums/bcrypt/enums.js'
 
 const getUser = async (id: number): Promise<UserPublic> => {
     return prisma.user.findUniqueOrThrow({
@@ -30,8 +26,8 @@ const updateNickname = async (
     data: NicknameUpdationData,
 ): Promise<UserPublic> => {
     return prisma.user.update({
-        where: { id },
-        data: { nickname: data.nickname },
+        where: {id},
+        data: {nickname: data.nickname},
         select: {
             id: true,
             nickname: true,
@@ -42,12 +38,12 @@ const updateNickname = async (
 
 const updatePassword = async (id: number, data: PasswordUpdationData) => {
     const user: User = await prisma.user.findUniqueOrThrow({
-        where: { id },
+        where: {id},
     })
 
     const isPasswordValid: boolean = await bcrypt.compare(
-        user.password,
         data.password,
+        user.password,
     )
     if (!isPasswordValid) {
         throw new AppError(
@@ -59,7 +55,7 @@ const updatePassword = async (id: number, data: PasswordUpdationData) => {
     const newHashedPassword = await bcrypt.hash(data.newPassword, SALT_ROUNDS)
 
     await prisma.user.update({
-        where: { id },
+        where: {id},
         data: {
             password: newHashedPassword,
         },
@@ -76,9 +72,9 @@ const getSettings = (userId: number): Promise<Settings> => {
 
 const updateSettings = (userId: number, data: SettingsUpdationData) => {
     return prisma.settings.update({
-        where: { userId },
+        where: {userId},
         data,
     })
 }
 
-export { getUser, updateNickname, updatePassword, getSettings, updateSettings }
+export {getUser, updateNickname, updatePassword, getSettings, updateSettings}
